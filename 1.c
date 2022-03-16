a7c644a8b1bbc6bf83cdf2d038510a0435d85a87
harshlad07 <74580401+harshlad07@users.noreply.github.com>
2022-03-16 11:22:00 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+	
+	execl("/home/trezen/Desktop/Linux/Linux Internals/assignment/test1","./test1", "test file", "discriptor", 0) ;
+	
+	printf("\n");
+	
+return 0;
+}
